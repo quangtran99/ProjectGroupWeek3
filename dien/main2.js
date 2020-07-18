aa15b0467c517e34fa3c886e78d2a9e2e100450c
Mã  Điền <madien@Mas-MacBook-Pro.local>
2020-07-18 14:40:11 +0700

    like

--- dien/main2.js
+++ dien/main2.js
@@ -47,7 +47,8 @@ function renderTweet(list) {
         <div class="dien-containerImage">
         <img src="https://cdn.discordapp.com/attachments/733234774646325309/733578515944833035/comment_icon.png" class="dien-imgTweet" />
         <div><i class="fas fa-retweet" style="cursor:pointer" onclick="retweet(${index})"data-toggle="modal" data-target="#myModal"></i></div>
-        <img src="https://cdn.discordapp.com/attachments/733234774646325309/733578519719968769/like_icon.png" class="dien-imgTweet" />
+        <div onclick="like(${index})"><i class="far fa-heart"></i>
+        </div>
         <div><i class="far fa-trash-alt " style="cursor:pointer" onclick="remove(${index})" ></i></div>
         
         </div>
@@ -55,6 +56,67 @@ function renderTweet(list) {
     `).join("");
     console.log(tweet);
     document.getElementById("tweetContent").innerHTML = b;
+}
+function like(index){
+    if (tweet[index].islike===true){
+        tweet[index].islike=false;
+    }else tweet[index].islike=true;
+    let b=[];
+    for (let i=0;i<tweet.length;i++){
+        if (tweet[i].islike==false){
+            b.push(` 
+            <div class="row dien-row1"style="  border:1px solid #e6ecf0;">
+                <div class="col-1 ">
+                    <img src="team-placeholder.png" class="dien-imgUser"
+                    style="width:40px;height:40px;">
+                </div>
+                <div class="col-11">
+                    <div style="width:100%;height:100%;">
+                        <span><b>Dien</b></span>
+                        <span style="color: #e6ecf0">@Dien a few seconds ago</span>
+                        <div class="dien-ContentTweet">${tweet[i].content}</div>  
+                    </div>
+                </div>
+                <div class="dien-containerImage">
+                <img src="https://cdn.discordapp.com/attachments/733234774646325309/733578515944833035/comment_icon.png" class="dien-imgTweet" />
+                <div><i class="fas fa-retweet" style="cursor:pointer" onclick="retweet(${i})"data-toggle="modal" data-target="#myModal"></i></div>
+                <div onclick="like(${i})"><i class="far fa-heart"></i>
+                </div>
+                <div><i class="far fa-trash-alt " style="cursor:pointer" onclick="remove(${i})" ></i></div>
+                
+                </div>
+            </div>
+            `);
+        }else{
+            b.push(` 
+            <div class="row dien-row1"style="  border:1px solid #e6ecf0;">
+                <div class="col-1 ">
+                    <img src="team-placeholder.png" class="dien-imgUser"
+                    style="width:40px;height:40px;">
+                </div>
+                <div class="col-11">
+                    <div style="width:100%;height:100%;">
+                        <span><b>Dien</b></span>
+                        <span style="color: #e6ecf0">@Dien a few seconds ago</span>
+                        <div class="dien-ContentTweet">${tweet[i].content}</div>  
+                    </div>
+                </div>
+                <div class="dien-containerImage">
+                <img src="https://cdn.discordapp.com/attachments/733234774646325309/733578515944833035/comment_icon.png" class="dien-imgTweet" />
+                <div><i class="fas fa-retweet" style="cursor:pointer" onclick="retweet(${i})"data-toggle="modal" data-target="#myModal"></i></div>
+                <div onclick="like(${i})"><i class="fas fa-heart"></i>
+                </div>
+                <div><i class="far fa-trash-alt " style="cursor:pointer" onclick="remove(${i})" ></i></div>
+                
+                </div>
+            </div>
+            `);
+        }
+    }
+    c=b.join("");
+    document.getElementById("tweetContent").innerHTML = b;
+
+
 }
 
 function dien_tweet(){
